20fa71444773bb2575c6542587cc61e07fd0eab4
Christiand666 <56047476+Christiand666@users.noreply.github.com>
2020-09-25 17:53:14 +0200

    lortett virker jo ike

---
+++ Lommeregnercore/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lommeregnercore.Models;
+
+namespace Lommeregnercore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Calculator cal)
+        {
+            int a, b;
+            a = cal.Value1;
+            b = cal.Value2;
+
+            if (cal.Calulate == "+")
+            {
+                cal.Resultat = a + b;
+            }
+            else if(cal.Calulate == "-")
+            {
+                cal.Resultat = a - b;
+            }
+            else if (cal.Calulate == "*")
+            {
+                cal.Resultat = a * b;
+            }
+            else if (cal.Calulate == "/")
+            {
+                cal.Resultat = a / b;
+            }
+
+            ViewData["result"] = cal.Resultat;
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lommeregnercore/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lommeregnercore.Models
+{
+    public class Calculator
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+
+        public int Resultat { get; set; }
+        public string Calulate { get; set; }
+
+    }
+}
